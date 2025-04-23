79d9b8d0699b44d756954b6eba8d7da254ca934a
timsinashok <ashoktimsina147181@gmail.com>
2025-04-23 09:17:36 +0400

    started new season

---
+++ Leetcode s2/242 - Valid Anagram.py
@@ -0,0 +1,14 @@
+"""
+Idea:
+use counter to get dict of characters
+and compare
+"""
+
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+
+        return counter_s == counter_t
