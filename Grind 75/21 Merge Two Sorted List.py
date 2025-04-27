e64f64b1be058c5d19ff0961b31574e08d4031d8
timsinashok <ashoktimsina147181@gmail.com>
2025-04-27 23:45:28 +0400

    added merge to sorted list

---
+++ Grind 75/21 Merge Two Sorted List.py
@@ -0,0 +1,26 @@
+"""
+compare the element one by one until one is done and then add the remaining
+"""
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(-1)  # Dummy starter
+        curr = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        # At this point, at least one list is empty
+        curr.next = list1 or list2
+
+        return dummy.next
